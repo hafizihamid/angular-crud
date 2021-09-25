16a0906a66ef45219436a8cd87b620ca4d91c024
hafizi hamid <mohamadhafizihamid@gmail.com>
2021-09-26 00:13:11 +0800

    feat: Add product interface

---
+++ src/app/model/product.ts
@@ -0,0 +1,29 @@
+export interface Product {
+  id: number;
+  name: string;
+  description: string;
+  price: string;
+  imageUrl: string;
+}
+
+// class Product implements IProduct {
+//   id!: number;
+//   name!: string;
+//   description!: string;
+//   price!: string;
+//   imageUrl!: string;
+
+//   constructor(
+//     id: number,
+//     name: string,
+//     description: string,
+//     price: string,
+//     imageUrl: string
+//   ) {
+//     this.id = id;
+//     this.name = name;
+//     this.description = description;
+//     this.price = price;
+//     this.imageUrl = imageUrl;
+//   }
+// }
